7b3f5282b7e41fdc676a7eb8cc69faeebb95ece4
Potted <ilorn.mc@gmail.com>
2022-08-26 12:32:40 +0800

    修正战斗中怪物体力内力不存在的问题

--- fqxy/pz/pz01.php
+++ fqxy/pz/pz01.php
@@ -13,17 +13,12 @@ $iniFile = new iniFile($ininame);
 $a5=$cmid;
 //将cmd最小最大值写入
 
-
 # 添加一个分类并直接添加子项
 $iniFile->addCategory('验证信息', ['xcmid值' => $a4, 'dcmid值' => $a5]);
 # 修改一个分类下子项的值(也可以修改多个)
 $iniFile->updItem('验证信息', ['xcmid值' => $a4,'dcmid值' => $a5]);
-
 //写入超链接及其所对应的值
-
-
 if($a4>=1){
-
     $i=-1;
     $q3=$cdid[$i+1];
     $q4=$clj[$i+1];
@@ -34,11 +29,7 @@ if($a4>=1){
     $iniFile->delCategory('超链接值');
     $iniFile->delCategory('超链接npc值');
 
-# 删除一个子项
-# 添加一个分类并直接添加子项
     $iniFile->addCategory('超链接值', [$q3 => $q4]);
-
-# 添加一个分类并直接添加子项
     $iniFile->addCategory('超链接npc值', [$q3 => $q6]);
 
     $aa=$a5-$a4+1;
@@ -47,43 +38,22 @@ if($a4>=1){
         $q3=$cdid[$i+1];
         $q4=$clj[$i+1];
         $q6=$npc[$i+1];
-# 添加一个子项(如果子项存在，则覆盖;)
         $iniFile->addItem('超链接值', [$q3 => $q4]);
         $iniFile->addItem('超链接npc值', [$q3 => $q6]);
         $i=$i+1;
-
     }
-
 } else{
-
     $iniFile->delCategory('超链接值');
     $iniFile->delCategory('超链接npc值');
-
-
-
     $aa=$a5-$a4+1;
     $i=-1;
-
     for($x=0;$x<$aa;$x++){
         $q3=$cdid[$i+1];
         $q4=$clj[$i+1];
         $q6=$npc[$i+1];
-# 添加一个子项(如果子项存在，则覆盖;)
         $iniFile->addItem('超链接值', [$q3 => $q4]);
         $iniFile->addItem('超链接npc值', [$q3 => $q6]);
         $i=$i+1;
-
     }
-
-
 }
 
-
-
-
-
-
-
-
-
-?>
--- fqxy/wj/sh01.php
+++ fqxy/wj/sh01.php
@@ -4,48 +4,30 @@
 //调用zt.ini是否存在
 include("./ini/zt_ini.php");
 include("./wj/ztt.php");
-
 include("./ini/zt_ini.php");
 $wjxx=($iniFile->getCategory('玩家信息'));
 //动态祝福加成数据
 include("./wj/zfzt.php");
-
 //获取返回战斗页面
 include("./ini/npc_ini.php");
 $yymid=($iniFile->getItem('怪物2号属性','初始'));
 $cljpost=$yymid;
 //获取返回战斗页面
 
-
-
-
-
-
 if($cljpost ==389){
-
-
-if($ncww02 >0){//优先获得宠物数据
-//获取玩家属性
-include("./ini/pkxx_ini.php");
-# 获取一个分类下所有数据$
-$guaiwu=($iniFile->getCategory('怪物2号属性'));
-
+    if($ncww02 >0){//优先获得宠物数据
+        //获取玩家属性
+        include("./ini/pkxx_ini.php");
+        $guaiwu=($iniFile->getCategory('怪物2号属性'));
+    } else{
+        include("./ini/pkxx_ini.php");
+        $guaiwu=($iniFile->getCategory('怪物1号属性'));
+    }
 } else{
-//获取玩家属性
-include("./ini/pkxx_ini.php");	
-# 获取一个分类下所有数据$
-$guaiwu=($iniFile->getCategory('怪物1号属性'));
-
-} 
-
-
-} else{
-//获取怪物属性
-include("./ini/npc_ini.php");	
-# 获取一个分类下所有数据$
-$guaiwu=($iniFile->getCategory('怪物1号属性'));
-} 
-
+    //获取怪物属性
+    include("./ini/npc_ini.php");
+    $guaiwu=($iniFile->getCategory('怪物1号属性'));
+}
 
 $gw1=$guaiwu['冰攻']-$wjxx1['冰防'];
 $gw2=$guaiwu['火攻']-$wjxx1['火防'];
@@ -61,45 +43,39 @@ $gg2=$gw4+$gw5+$gw6;
 include("./ini/jn_ini.php");
 $jnlvl=($iniFile->getItem('技能等级',$npcc));
 if($jnlvl ==1){
-$jnsh=1;
-$kcmp=10;
+    $jnsh=1;
+    $kcmp=10;
 } elseif($jnlvl ==2){
-$jnsh=1.1;
-$kcmp=15;
+    $jnsh=1.1;
+    $kcmp=15;
 } elseif($jnlvl ==3){
-$jnsh=1.2;
-$kcmp=20;
+    $jnsh=1.2;
+    $kcmp=20;
 } elseif($jnlvl ==4){
-$jnsh=1.4;
-$kcmp=25;
+    $jnsh=1.4;
+    $kcmp=25;
 } elseif($jnlvl ==5){
-$jnsh=1.5;
-$kcmp=30;
-} elseif($jnlvl ==6){	
-$jnsh=1.6;
-$kcmp=35;
+    $jnsh=1.5;
+    $kcmp=30;
+} elseif($jnlvl ==6){
+    $jnsh=1.6;
+    $kcmp=35;
 } elseif($jnlvl ==7){
-$jnsh=1.7;
-$kcmp=40;
-} elseif($jnlvl ==8){	
-$jnsh=1.8;
-$kcmp=45;
-} elseif($jnlvl ==9){	
-$jnsh=1.9;
-$kcmp=50;
+    $jnsh=1.7;
+    $kcmp=40;
+} elseif($jnlvl ==8){
+    $jnsh=1.8;
+    $kcmp=45;
+} elseif($jnlvl ==9){
+    $jnsh=1.9;
+    $kcmp=50;
 } elseif($jnlvl ==10){
-$jnsh=2;
-$kcmp=60;
+    $jnsh=2;
+    $kcmp=60;
 } elseif($jnlvl ==11){
-$jnsh=2.1;
-$kcmp=70;
+    $jnsh=2.1;
+    $kcmp=70;
 } else{
-$jnsh=1;
+    $jnsh=1;
 }
-
-
-
-
-?>
-
 
