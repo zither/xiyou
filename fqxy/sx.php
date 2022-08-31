b71ab902d03fe5ecfb658d4fe1119de1ef5fce48
Potted <ilorn.mc@gmail.com>
2022-08-31 16:06:53 +0800

    修复登录失效页面缺失

--- fqxy/sx.php
+++ fqxy/sx.php
@@ -2,12 +2,9 @@
 ///////////////////////////////////游戏失效页面//////////////////////////////////////////
 //echo "<font color=black>当前登录信息已失效请重新</font>"."<a href='xxjyindex.php'><font color=blue>登录</font></a>"."<br>";
 
-include("xxjyindex.php");
-	
-
-
-?>
-
-
-
+$suffix = config_item('jy_enable_https') ? 's' : '';
+$host = config_item('host');
+echo "登录信息已失效，请重新登录<br>";
+echo "<a href='http$suffix://$host'>返回首页</a>";
+exit;
 
