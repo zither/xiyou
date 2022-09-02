33fa15cef286dffeeff1aad3c3b79e41a0af1d3d
Potted <ilorn.mc@gmail.com>
2022-09-02 12:47:38 +0800

    修改数据库配置

--- fqxy/sql/mysql_example.php
+++ fqxy/sql/mysql_example.php
@@ -1,10 +1,12 @@
 <?php
 
+include_once __DIR__ . '/db.php';
+
 $mysql_host = 'mysql';
 $mysql_user = 'root';
 $mysql_password = 'root';
 $mysql_database = 'xyy';
 
-$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database) or die ("连接服务器失败1");
-mysqli_query($conn, "set names utf8");
+$conn = DB::init($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+
 include_once dirname(__DIR__) . "/../includes/wrappers.php";
