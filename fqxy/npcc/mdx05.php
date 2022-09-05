2f67332170cf47f2ef25671f7387d0c0af35f1b1
Potted <ilorn.mc@gmail.com>
2022-09-05 18:40:04 +0800

    修复果子商城购买报错

--- fqxy/npcc/mdx05.php
+++ fqxy/npcc/mdx05.php
@@ -48,9 +48,13 @@ echo "<font color=black>对不起".$wpmz."价格有误，联系Gm解决</font>".
 
 echo "<font color=black>请输入你要购买多少".$wpmz."呢？</font>"."<br>";
 
-
+$cmid = $cmid + 1;
+$cdid[] = $cmid;
+$clj[] = 423;
+$npc[] = $npcc;
+$mdxurl = sprintf('xy.php?uid=%d&cmd=%d&sid=%s', $wjid, $cmid, $a1);
 ?>
-<form  action="" method="POST">
+<form  action="<?php echo $mdxurl?>" method="POST">
 <input type="tel" name="sl" placeholder="请输入"id='search'onkeyup="this.value=this.value.replace(/\D/g,'')" onafterpaste="this.value=this.value.replace(/\D/g,'')"><br>
 <input  type="submit" name="submit"  value="购买" id="search1"><br>
 </form>
