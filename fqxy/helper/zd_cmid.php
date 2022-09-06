8acfe36c6d7bd6552b2f70abb31f4e994ca6b4bf
Potted <ilorn.mc@gmail.com>
2022-09-06 09:54:03 +0800

    修复npc购物报错

--- fqxy/helper/zd_cmid.php
+++ fqxy/helper/zd_cmid.php
@@ -5,7 +5,7 @@ function zd_cmid($wjid, $cmid = null) {
         echo "无效玩家编号 #1";
         exit;
     }
-    include("./ini/npc_ini.php");
+    include(XY_DIR . "/ini/npc_ini.php");
     if (!is_null($cmid)) {
         $iniFile->updItem('怪物2号属性', ['初始' => $cmid]);
         return $cmid;
