5e918a20c9eae8c1d26bd7d73aee0fbd17a71726
Potted <ilorn.mc@gmail.com>
2022-09-07 12:57:49 +0800

    删除充值代码和样式代码

--- admin/login.php
+++ admin/login.php
@@ -8,47 +8,8 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
     <title>幻想西游管理平台-登录</title>
-    <link rel="stylesheet" href="../css/touchScreen/mobile.css" />
-    <link rel="stylesheet" href="../css/touchScreen/ch5game.css" />
-    <link rel="stylesheet" href="../css/touchScreen/ccolorbutton.css" />
     <link rel="icon" href="../img/favicon.png" type="image/x-icon" />
     <style>
-        #search{
-            width:235px;
-            height:32px;
-            border-radius:10px;
-            margin-top:5px;
-            margin-bottom:5px;
-            border:1px solid#ccc;
-            background-color:transparent;
-            transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-        }
-        #search1{
-            width:82px;
-            height:38px;
-            background:#0086DB;
-            border:0;
-            border-radius:10px;
-            margin-top:5px;
-            color:white;
-            font-size:16px;
-            cursor:pointer;
-        }
-        input:focus{
-            border-color:#66AFE9!important;
-            outline:0;
-            -webkit-box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,0.6);
-            -moz-box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,0.6);
-            box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,0.6);
-        }
-        ::-webkit-input-placeholder{
-            text-indent:20px;
-            font-size:16px;
-        }
-        ::-moz-input-placeholder{
-            text-indent:20px;
-            font-size:16px;
-        }
 
     </style>
 </head>
@@ -106,24 +67,16 @@ try {
 }
 ?>
 
-<div class="g-mn">
-    <div class="m-box">
-        <div class="m-box-hd">
-            <div class="tt">
-                <?php echo $zcxx;?><br>
-                请输入管理员账号和密码
-            </div>
-        </div>
-        <div class="m-box-mn" style="padding: 5px 10px 0px 10px;">
-            <form  action="login.php" method="POST">
-                账号：<input  type="text" name="username" placeholder="请输入管理员账号"id='search'><br>
-                密码：<input  type="password" name="password" placeholder="请输入管理员密码"id='search'><br>
-                <input  type="submit" name="submit" value="登录"  id="search1" ><br>
-            </form>
-            <div class="article-content" style="font-size: 15px;margin-top: 10px;">
-            </div>
-        </div>
-    </div>
+<div>
+    <?php echo $zcxx;?><br>
+    请输入管理员账号和密码
+</div>
+<div>
+    <form  action="login.php" method="POST">
+        账号：<input  type="text" name="username" placeholder="请输入管理员账号"id='search'><br>
+        密码：<input  type="password" name="password" placeholder="请输入管理员密码"id='search'><br>
+        <input  type="submit" name="submit" value="登录"  id="search1" ><br>
+    </form>
 </div>
 
 <div style="min-height: 80px;">
--- fqxy/xy.php
+++ fqxy/xy.php
@@ -6,48 +6,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>幻想西游</title>
     <link rel="shortcut icon" href="pic/ico/favicon.ico"/>
-    <style>
-        #search {
-            width: 235px;
-            height: 32px;
-            border-radius: 10px;
-            margin-top: 5px;
-            margin-bottom: 5px;
-            border: 1px solid #ccc;
-            background-color: transparent;
-            transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-        }
-
-        #search1 {
-            width: 82px;
-            height: 38px;
-            background: #0086DB;
-            border: 0;
-            border-radius: 10px;
-            margin-top: 5px;
-            color: white;
-            font-size: 16px;
-            cursor: pointer;
-        }
-
-        input:focus {
-            border-color: #66AFE9 !important;
-            outline: 0;
-            -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, 0.6);
-            -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, 0.6);
-            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, 0.6);
-        }
-
-        ::-webkit-input-placeholder {
-            text-indent: 20px;
-            font-size: 16px;
-        }
-
-        ::-moz-input-placeholder {
-            text-indent: 20px;
-            font-size: 16px;
-        }
-    </style>
 </head>
 <body>
 <div style='width: device-width;display:block;word-break: break-all;word-wrap: break-word;'>
