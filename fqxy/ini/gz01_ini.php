1fa51663a86d72caace149fd4a76aa606db5495f
Potted <ilorn.mc@gmail.com>
2022-09-09 21:39:18 +0800

    修正查看攻城信息

--- fqxy/ini/gz01_ini.php
+++ fqxy/ini/gz01_ini.php
@@ -1,38 +1,16 @@
 <?php
 
-//判断ini文件是否存在如不没有则从数据库提取并且写入ini
-$inina="gz01.ini";
-$path='acher/guoz';
-$file = $path."/".$inina;
-if(file_exists($file)) {
+$iniFile = new iniFile('__FAKE_FILE__', true);
+$db = DB::instance();
+$gz01_arr = $db->select('gz01', '*');
 
-
-} else {
-    $inina="gz01.ini";
-    $path='acher/guoz';
-    $file = $path."/".$inina;
-    file_put_contents($file,"[国战信息]");
-    $iniFile = new iniFile($file);
-    $iniFile->addItem('id',['初始' => 0]);
-    $iniFile->addItem('国家名字',['初始' => 0]);
-    $iniFile->addItem('国家id',['初始' => 0]);
-    $iniFile->addItem('君主名字',['初始' => 0]);
-    $iniFile->addItem('君主id',['初始' => 0]);
-    $iniFile->addItem('夺仗人',['初始' => 0]);
-    $iniFile->addItem('夺仗人id',['初始' => 0]);
-
-    $q2="gz01";
-    $str="select * from $q2";
-    $result=mysql_query($str) or die('SQL语句有误');
-    while(!!$row=mysql_fetch_array($result)){
-        $iniFile->addCategory('id', [$row['zcid']=>$row['zlgj']]);
-        $iniFile->addCategory('国家名字', [$row['zcid']=>$row['zlgj']]);
-        $iniFile->addCategory('国家id', [$row['zcid']=>$row['zlgjid']]);
-        $iniFile->addCategory('君主名字', [$row['zcid']=>$row['gjjz']]);
-        $iniFile->addCategory('君主id', [$row['zcid']=>$row['gjjzid']]);
-        $iniFile->addCategory('夺仗人', [$row['zcid']=>$row['czz']]);
-        $iniFile->addCategory('夺仗人id', [$row['zcid']=>$row['czzid']]);
-    }
+foreach ($gz01_arr as $row) {
+    $iniFile->addItem('id', [$row['zcid']=>$row['zlgj']]);
+    $iniFile->addItem('国家名字', [$row['zcid']=>$row['zlgj']]);
+    $iniFile->addItem('国家id', [$row['zcid']=>$row['zlgjid']]);
+    $iniFile->addItem('君主名字', [$row['zcid']=>$row['gjjz']]);
+    $iniFile->addItem('君主id', [$row['zcid']=>$row['gjjzid']]);
+    $iniFile->addItem('夺仗人', [$row['zcid']=>$row['czz']]);
+    $iniFile->addItem('夺仗人id', [$row['zcid']=>$row['czzid']]);
+    $iniFile->addItem('占领时间', [$row['zcid']=>$row['zlsj']]);
 }
-
-$iniFile = new iniFile($file);
