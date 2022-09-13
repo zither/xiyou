2fd5928444f5e1c69317ca20ee9ac38d3eb93c73
Potted <ilorn.mc@gmail.com>
2022-09-13 10:14:29 +0800

    新增坐标传送

---
+++ fqxy/template/xy691.php
@@ -0,0 +1,19 @@
+<?php
+
+//阻塞代码防止出现脏数据
+$ininalock=$wjid."_lock".".txt";
+include("./ini/zsini.php");
+
+if($zsspd==1) {
+    //传送玩家到指定坐标
+    $kknpcc = $npcc;
+    $arr = explode('_', $npcc);
+    if (count($arr) == 2) {
+        wjyd($wjid, (int)$arr[0], (int)$arr[1]);
+    }
+    include XY_DIR . '/template/xy002.php';
+}
+
+//解锁当前使用的ini
+include("./ini/jsini.php");
+//解锁当前使用的ini
