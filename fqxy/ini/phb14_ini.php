f4a4df8ccdeac03e50b4c77b9bfd77eb788a9890
Potted <ilorn.mc@gmail.com>
2022-09-19 09:01:49 +0800

    现有排行榜不再依赖文件缓存

--- fqxy/ini/phb14_ini.php
+++ fqxy/ini/phb14_ini.php
@@ -1,34 +1,21 @@
 <?php
 
-//判断ini文件是否存在如不没有则从数据库提取并且写入ini
-$inina="phb14.ini";
-$path='acher/phb';
-$file = $path."/".$inina;
-if(file_exists($file)) {
+$iniFile = new iniFile('__PHB14_INI__', true);
+$iniFile->addItem('排行榜名字',['初始' => 123]);
+$iniFile->addItem('排行榜值1',['初始' => 0]);
+$iniFile->addItem('排行榜值2',['初始' => 0]);
+$iniFile->addItem('排行榜值3',['初始' => 0]);
+$iniFile->addItem('排行榜值4',['初始' => 0]);
 
-} else {
-
-    $inina="phb14.ini";
-    $path='acher/phb';
-    $file = $path."/".$inina;
-    file_put_contents($file,"[排行榜信息]");
-    $iniFile = new iniFile($file);
-    $iniFile->addItem('排行榜名字',['初始' => 123]);
-    $iniFile->addItem('排行榜值1',['初始' => 0]);
-    $iniFile->addItem('排行榜值2',['初始' => 0]);
-    $iniFile->addItem('排行榜值3',['初始' => 0]);
-    $iniFile->addItem('排行榜值4',['初始' => 0]);
-
-    $q2="all_hdph02";
-    $str="select * from $q2";
-    $result=mysql_query($str) or die('SQL语句有误');
-    while(!!$row=mysql_fetch_array($result)){
-        $iniFile->addCategory('排行榜名字', [$row['id']=> $row['wjmz']]);
-        $iniFile->addCategory('排行榜值1', [$row['id']=> $row['wjid']]);
-        $iniFile->addCategory('排行榜值2', [$row['id']=> $row['ds01']]);
-        $iniFile->addCategory('排行榜值3', [$row['id']=> $row['vip']]);
-        $iniFile->addCategory('排行榜值4', [$row['id']=> $row['id']]);
-    }
+$db = DB::instance();
+$phb1_data = $db->select('all_hdph02', ['id', 'wjid', 'wjmz', 'vip', 'ds01'], [
+    'ORDER' => ['ds01' => 'DESC'],
+    'LIMIT' => 10,
+]);
+foreach ($phb1_data as $row) {
+    $iniFile->addCategory('排行榜名字', [$row['id']=> $row['wjmz']]);
+    $iniFile->addCategory('排行榜值1', [$row['id']=> $row['wjid']]);
+    $iniFile->addCategory('排行榜值2', [$row['id']=> $row['ds01']]);
+    $iniFile->addCategory('排行榜值3', [$row['id']=> $row['vip']]);
+    $iniFile->addCategory('排行榜值4', [$row['id']=> $row['id']]);
 }
-
-$iniFile = new iniFile($file);
