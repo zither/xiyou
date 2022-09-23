6141968583c96d8558a5561a6188d3f9d6152e31
Potted <ilorn.mc@gmail.com>
2022-09-23 12:30:47 +0800

    挂售可以正常下架了

--- fqxy/npcc/gsxjwp01.php
+++ fqxy/npcc/gsxjwp01.php
@@ -1,40 +1,19 @@
 <?php
 
-
-
-
-
 $hff=1;
 $dqwp=0;
 
 if($hff==1){
-if($_POST['submit']){
+    if($_POST['submit']){
 
-$sl= $_POST['sl'];
-$sll=preg_match('/^\d+$/i', $sl);
-$dqwp=1;
-include("gsxjwp02.php");
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
+        $sl= $_POST['sl'];
+        $sll=preg_match('/^\d+$/i', $sl);
+        $dqwp=1;
+        include("gsxjwp02.php");
+    }
 }
 if($dqwp==1){
-exit();
+    exit();
 }
 
 echo "<font color=red>你最多可下架".$wpmz."x".$wpsl."</font>"."<br>";
@@ -56,15 +35,20 @@ $clj[]=219;
 $npc[]=0;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回物品挂售</font></a>"."<br>";
 
+$cmid=$cmid+1;
+$cdid[]=$cmid;
+$clj[]=221;
+$npc[]=$npcc;
+$formurl = sprintf('xy.php?uid=%d&cmd=%d&sid=%s', $wjid, $cmid, $a1);
+
 ?>
-<form  action="" method="POST">
-<input type="tel" name="sl" placeholder="请输入"id='search'onkeyup="this.value=this.value.replace(/\D/g,'')" onafterpaste="this.value=this.value.replace(/\D/g,'')"><br>
-<input  type="submit" name="submit"  value="下架" id="search1"><br>
+<form  action="<?php echo $formurl?>" method="POST">
+    <input type="tel" name="sl" placeholder="请输入"id='search'onkeyup="this.value=this.value.replace(/\D/g,'')" onafterpaste="this.value=this.value.replace(/\D/g,'')"><br>
+    <input  type="submit" name="submit"  value="下架" id="search1"><br>
 </form>
 <?php
 echo "<br>";
 echo "<br>";
-?>
 
 
 
